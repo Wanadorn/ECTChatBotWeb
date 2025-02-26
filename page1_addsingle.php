4840abe10368e95a03ed4f94398850a5803e1d7c
Wanadorn <152370529+Wanadorn@users.noreply.github.com>
2025-02-27 02:20:04 +0700

    v.1.0

--- page1_addsingle.php
+++ page1_addsingle.php
@@ -1,46 +1,56 @@
-<form action="page1_add.php" method="post">   
-    <div class="mb-3">                                                         
-        <div class="input-group mb-3">                                  
-            <span class="input-group-text">รหัสวิชา</span>
-            <input type="text" class="form-control" name="id" required>   
-        </div>
-        <div class="input-group mb-3">
-            <span class="input-group-text">รายชื่อวิชา</span>                             
-            <input type="text" class="form-control" name="name" required>
-        </div>
-        <div class="input-group mb-3">
-            <span class="input-group-text">หน่วยกิต</span>                                
-            <input type="text" class="form-control" name="credit" required>
-        </div>
+<form action="page1_add.php" method="post">     
+    <div class="modal" id="ModalSingle">
+        <div class="modal-dialog">                
+            <div class="modal-content">   
+                <div class="modal-header"> 
+                    <h1 class="modal-title fs-5" id="ModalLabel">กรุณากรอกข้อมูล</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">                                               
+                    <div class="mb-3">                                                         
+                        <div class="input-group mb-3">                                  
+                            <span class="input-group-text">รหัสวิชา</span>
+                            <input type="text" class="form-control" name="id" required>   
+                        </div>
+                        <div class="input-group mb-3">
+                            <span class="input-group-text">รายชื่อวิชา</span>                             
+                            <input type="text" class="form-control" name="name" required>
+                        </div>
+                        <div class="input-group mb-3">
+                            <span class="input-group-text">หน่วยกิต</span>                                
+                            <input type="text" class="form-control" name="credit" required>
+                        </div>
 
-        <div class="input-group mb-3">
-            <span class="input-group-text">ภาษา</span>                            
-            <input type="text" class="form-control" name="language" required>
+                        <div class="input-group mb-3">
+                            <span class="input-group-text">ภาษา</span>                            
+                            <input type="text" class="form-control" name="language" required>
 
-            <span class="input-group-text">ปี</span>                                 
-            <input type="text" class="form-control" name="year" required>                             
+                            <span class="input-group-text">ปี</span>                                 
+                            <input type="text" class="form-control" name="year" required>                             
+                        </div>
+
+                        <div class="input-group mb-3">                        
+                            <span class="input-group-text">เทอม</span>                                
+                            <input type="text" class="form-control" name="term" required>
+
+                            <span class="input-group-text">ปีการศึกษา</span> 
+                            <select name="course_year" class="form-select">
+                                <?php
+                                    $conn=new PDO("mysql:host=localhost;dbname=ect_chatbot;charset=utf8","root","");
+                                    $sql="SELECT * FROM course_year";
+                                    foreach($conn->query($sql) as $row){
+                                        echo "<option value=$row[0]>$row[1]</option>";
+                                    }
+                                    $conn=null;
+                                ?>                                                
+                            </select>                                                                                                           
+                        </div>                     
+                    </div>                                                                                                                                                                                                                            
+                </div>
+                <div class="modal-footer">      
+                    <button type="submit" class="btn btn-success mx-auto">บันทึก</button>
+                </div>                                  
+            </div>          
         </div>
-
-        <div class="input-group mb-3">                        
-            <span class="input-group-text">เทอม</span>                                
-            <input type="text" class="form-control" name="term" required>
-
-            <span class="input-group-text">ปีการศึกษา</span> 
-            <select name="course_year" class="form-select">
-                <?php
-                    $conn=new PDO("mysql:host=localhost;dbname=ect_chatbot;charset=utf8","root","");
-                    $sql="SELECT * FROM course_year";
-                    foreach($conn->query($sql) as $row){
-                        echo "<option value=$row[0]>$row[1]</option>";
-                    }
-                    $conn=null;
-                ?>                                                
-            </select>
-                                                                                                            
-        </div>                                                                                                        
-    </div>        
-                 
-    <div class="modal-footer">      
-        <button type="submit" class="btn btn-success mx-auto">บันทึก</button>
-    </div>
+    </div>   
 </form>
